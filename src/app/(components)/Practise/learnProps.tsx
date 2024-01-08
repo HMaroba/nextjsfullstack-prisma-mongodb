76b7beedc93af31c6e67fb9f197ac7536622c4e7
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-08 16:16:37 +0200

    Added props

--- src/app/(components)/Practise/learnProps.tsx
+++ src/app/(components)/Practise/learnProps.tsx
@@ -2,10 +2,16 @@
 
 import React from 'react'
 
-const LearnProps = () => {
+type Props = {
+    name : string;
+    gender : 'male' | 'female',
+    phoneNumber : string
+}
+
+const LearnProps = ({name, gender, phoneNumber} : Props) => {
   return (
     <div>
-      
+      <p>My name is {name} , my phone number is {phoneNumber} and i am {gender}</p>
     </div>
   )
 }
