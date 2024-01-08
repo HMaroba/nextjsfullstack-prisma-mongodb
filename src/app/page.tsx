c196dc4787092053c93c506833355de03b1d0408
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-08 16:17:32 +0200

    Added props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import LearnProps from "./(components)/Practise/learnProps";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>FULLSTACK APP USING MONGODB , NEXT JS , PRISMA</p>
+      <LearnProps name="Hlalele Maroba" gender="male" phoneNumber="56891234" />
     </main>
   );
 }
