7bdc4b15e11a0e6fba1ff573ec12a6691804d045
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-08 13:22:07 +0200

    Formatted button

--- src/app/(components)/FormComponents/Button.tsx
+++ src/app/(components)/FormComponents/Button.tsx
@@ -4,17 +4,17 @@ type ButtonProps = {
   name: string;
 };
 
-function Button() {
+const CustomButton = ({ name }: ButtonProps) => {
   return (
     <div>
       <button
         className="block mx-auto mb-6 px-12 h-10 bg-slate-900 text-white hover:bg-slate-700 transition-colors rounded-md w-4/5"
         type="submit"
       >
-        Login
+        {name}
       </button>
     </div>
   );
-}
+};
 
-export default Button;
+export default CustomButton;
