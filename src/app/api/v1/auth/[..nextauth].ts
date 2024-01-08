27b06ddd66ad80ffc104f4d4b2d8de909cc8e4ff
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-08 11:47:47 +0200

    Added next auth provider

--- src/app/api/v1/auth/[..nextauth].ts
+++ src/app/api/v1/auth/[..nextauth].ts
@@ -1,6 +1,11 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { PrismaClient } from "@prisma/client";
+import { compare } from "bcryptjs";
+const prisma = new PrismaClient();
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -10,11 +15,31 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET!,
     }),
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-      })
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      async authorize(credentials, req) {
+        const { emailAddress, password } = credentials as {
+          emailAddress: string;
+          password: string;
+        };
+
+        const user = await prisma.employee.findFirst({
+          where: { emailAddress },
+        });
+        if (!user) throw new Error("No user Found with Email");
+
+        const checkPassword = await compare(password, user?.password as string);
+        if (!checkPassword) throw new Error("Password not matched");
+
+        return user;
+      },
+      credentials: {},
+    }),
     // ...add more providers here
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
