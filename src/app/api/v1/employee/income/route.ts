64df04350511eb53ee5061e02f0953099374e168
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-08 11:18:49 +0200

    Created get income api

--- src/app/api/v1/employee/income/route.ts
+++ src/app/api/v1/employee/income/route.ts
@@ -1,15 +1,17 @@
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    const getIncomes = await prisma?.income.findMany();
+    const getIncomes = await prisma.income.findMany();
     return NextResponse.json({
       incomes: getIncomes,
       success: true,
     });
   } catch (error) {
     return NextResponse.json({
-      message: "Something went wrong",
+      message: "Something went wrong" + error,
       status: 500,
     });
   }
