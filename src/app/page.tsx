85ceb850a9072e53d0e96b1755758b8825d6bc5b
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-09 08:49:47 +0200

    Added account model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,76 @@
-import Image from "next/image";
-import LearnProps from "./(components)/Practise/learnProps";
+"use client";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [selectedRole, setSelectedRole] = useState("");
+  const [customRole, setCustomRole] = useState("");
+
+  const roles = [
+    { name: "Developer" },
+    { name: "Accountant" },
+    { name: "Marketing" },
+    { name: "Manager" },
+    { name: "Supervisor" },
+  ];
+
+  const handleRoleChange = (event: any) => {
+    setSelectedRole(event.target.value);
+  };
+
+  const handleCustomRoleChange = (event: any) => {
+    setCustomRole(event.target.value);
+  };
+
+  const handleAddCustomRole = () => {
+    if (
+      customRole.trim() !== "" &&
+      !roles.some((role) => role.name === customRole)
+    ) {
+      // Add the custom role to the list of roles
+      roles.push({ name: customRole });
+      // Clear the custom role input
+      setCustomRole("");
+      console.log(roles);
+      
+    }
+  };
+
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <p>FULLSTACK APP USING MONGODB , NEXT JS , PRISMA</p>
-      <LearnProps name="Hlalele Maroba" gender="male" phoneNumber="56891234" />
+      <div>
+        <p>FULLSTACK APP USING MONGODB, NEXT JS, PRISMA</p>
+        <div className="mt-4 w-full">
+          <label className="space-x-4 mr-4">User Role</label>
+          <select
+            className="border-3 border-blue-500 rounded-md w-full h-8"
+            value={selectedRole}
+            onChange={handleRoleChange}
+          >
+            {roles.map((item, index) => (
+              <option key={index} value={item.name}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* Input field for custom role */}
+        <div className="mt-4 w-full">
+          <label className="space-x-4 mr-4">Custom Role</label>
+          <input
+            type="text"
+            value={customRole}
+            onChange={handleCustomRoleChange}
+            className="border-3 border-blue-500 rounded-md w-full h-8 p-2"
+          />
+          <button
+            onClick={handleAddCustomRole}
+            className="px-2 py-1 mt-4 bg-blue-500 text-white rounded-md"
+          >
+            Add Custom Role
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
