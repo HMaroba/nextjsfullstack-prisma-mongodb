c2d60e13d48565f50f1d8f60c2ef5541616d2a25
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-12 16:46:06 +0200

    Updated prisma and client

--- src/app/api/v1/contact/route.ts
+++ src/app/api/v1/contact/route.ts
@@ -4,9 +4,16 @@ const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   try {
-    const {email , phoneNumber, firstName, lastName} = await req.json();
+    const { email, phoneNumber, firstName, lastName } = await req.json();
 
-    const userExits = await pris
+    const userExits = await prisma.contact.findUnique({ where: { email } });
+    if (userExits) {
+      return NextResponse.json({
+        success: false,
+        message: "User already exists",
+        status: 422,
+      });
+    }
   } catch (error) {
     return NextResponse.json({
       message: "Error creating contact",
