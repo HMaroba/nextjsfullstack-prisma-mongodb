4c7d1848075dcd8c1fabf19632df7fcb2d4200ea
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-15 16:31:05 +0200

    Added attemps and blocked to account

--- src/app/api/v1/auth/login/route.ts
+++ src/app/api/v1/auth/login/route.ts
@@ -33,7 +33,7 @@ export async function POST(request: NextRequest) {
       const timeDifferenceInMinutes = (currentTime - blockTime) / (1000 * 60);
 
       // Check if 30 minutes have passed since the account was blocked
-      if (timeDifferenceInMinutes >= 10) {
+      if (timeDifferenceInMinutes >= 5) {
         // Unblock the account
         await prisma.employee.update({
           where: { id: user.id },
@@ -44,7 +44,7 @@ export async function POST(request: NextRequest) {
           },
         });
       } else {
-        const minutesLeft = Math.ceil(10 - timeDifferenceInMinutes);
+        const minutesLeft = Math.ceil(5 - timeDifferenceInMinutes);
         return NextResponse.json(
           // { error: "Account is blocked. Please contact support." },
           {
