0731737636172bc075633150ca79678d12c6b7a6
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-15 07:45:58 +0200

    added React quesry

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,37 @@
+"use client";
 import React from "react";
+import { useQuery } from "react-query";
 
 export default function Dashboard() {
+  const getEmployees = async () => {
+    const response = await fetch("/api/v1/employee");
+    return response.json();
+  };
+
+  const {
+    data: userData,
+    isLoading,
+    isError,
+  } = useQuery("userData", getEmployees);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <p className="text-2xl">Loading please wait ...</p>;
+      </div>
+    );
+  }
+
+  if (isError) {
+    console.log(isError);
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <p className="text-2xl">Error fetching data</p>;
+      </div>
+    );
+  }
+
+  console.log(userData);
+  
   return <div></div>;
 }
