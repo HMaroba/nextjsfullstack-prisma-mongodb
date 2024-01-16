8e9b3c359820ba8578dee746ce153499833fb758
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-16 10:12:00 +0200

    Mododified  ui

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <div className='bg-gray-200 p-10 h-screen'>
+     <div className='p-2 bg-white min-h-40'>
+        <p>Contact us page</p>
+     </div>
+    </div>
+  )
+}
