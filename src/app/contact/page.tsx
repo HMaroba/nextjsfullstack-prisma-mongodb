7aee97f7e6563d99dbe637f2bfa9cb51101bda99
Coffee58511867 <marobahlalele910@gmail.com>
2024-01-16 10:46:05 +0200

    Added some fields

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,13 +3,22 @@ import React, { useState } from "react";
 
 export default function ContactPage() {
   const [showMore, setShowMore] = useState(false);
+
+  const show = () => {
+    setShowMore(!showMore);
+  };
   return (
     <div className="bg-gray-200 p-10 h-screen">
       <div className="p-2 bg-white min-h-40">
         <p>Contact us page</p>
-        <button className="mt-4 text-white bg-black rounded-md p-2">
+        <button className="mt-4 text-white bg-black rounded-md p-2 hover:rounded-sm">
           Check More
         </button>
+        {showMore ? (
+          <div>
+            <p>Are you ready child</p>
+          </div>
+        ) : null}
       </div>
     </div>
   );
